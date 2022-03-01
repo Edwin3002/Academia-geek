6565dde57a9d839a044c7e51cbeb0d56838c6c03
Edwin <vargas11742@gmail.com>
2022-03-01 11:31:30 -0500

    dar valores a la tabla acerca de los planetas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let nPlanetas = Array('Tierra', 'Marte', 'luna');
-let distancia = [ 6,5, '69'];
+let distancia = [ 6,5, 7];
 const tamaño = [];
 console.log(nPlanetas)
 console.log(distancia)
@@ -12,11 +12,26 @@ while(i <= nPlanetas.length -1){
     i = i + 1
 }
 
-for (const k in distancia){
-    if(distancia[k] == '5'){
-        console.log('Si se encontro la distancia')
-    }else{
-        console.log('No se encontro la distancia')
+// for (const k in distancia){
+//     if(distancia[k] == '5'){
+//         console.log('Si se encontro la distancia')
+//     }else{
+//         console.log('No se encontro la distancia')
 
-    }
-}
+//     }
+// }
+nPlanetas.forEach((p, i) =>{
+    console.log("planeta", p, i)
+    document.getElementById(`nPlaneta_${i}`).value = i;
+    document.getElementById(`planeta_${i}`).value = p;
+    // document.getElementById(`dPlaneta_${i}`).value = i;
+    console.log(i)
+})
+distancia.forEach((d, i) =>{
+    console.log("distancia", d)
+    document.getElementById(`dPlaneta_${i}`).value = d;
+    document.getElementById(`tPlaneta_${i}`).value = d*5;
+    console.log(i)
+})
+// let as = document.getElementById
+// console.log(nPlaneta_1)
